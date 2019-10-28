6a04b122ab498c2afb355e2cd77dee8402fa1fe2
Jdpa357 <jdpa352@hotmail.es>
2019-10-28 15:52:46 +0000

    Adding more functions to file, and indenting the code

--- printf_functions.c
+++ printf_functions.c
@@ -12,7 +12,7 @@
 
 void _putchar(char c)
 {
-write(1, &c, 1);
+	write(1, &c, 1);
 }
 
 /**
@@ -25,8 +25,8 @@ write(1, &c, 1);
 int char_print(va_list args)
 {
 
-_putchar(va_arg(args, int));
-return (1);
+	_putchar(va_arg(args, int));
+	return (1);
 }
 
 /**
@@ -38,21 +38,21 @@ return (1);
 
 int str_print(va_list args)
 {
-int i, j;
-char *s;
+	int i, j;
+	char *s;
 
-s = va_arg(args, char *);
-if (s == NULL)
-s = "(NULL)";
-if (s[0] == '\0')
-return (0);
-for (i = 0; s[i] != '\0'; i++)
-;
-for (j = 0; j < i; j++)
-{
-_putchar(s[j]);
-}
-return (i);
+	s = va_arg(args, char *);
+	if (s == NULL)
+		s = "(NULL)";
+	if (s[0] == '\0')
+		return (0);
+	for (i = 0; s[i] != '\0'; i++)
+		;
+	for (j = 0; j < i; j++)
+	{
+		_putchar(s[j]);
+	}
+	return (i);
 }
 /**
  * int_print - Function that prints integers to standard output
@@ -64,32 +64,103 @@ return (i);
 int int_print(va_list args)
 {
 
-long int num, div, i = 0;
+	long int num, div, i = 0;
 
-num = va_arg(args, int);
+	num = va_arg(args, int);
 
-if (num == 0)
-{
-_putchar('0');
-i = 1;
+	if (num == 0)
+	{
+		_putchar('0');
+		i = 1;
+	}
+	else if (num < 0)
+	{
+		_putchar('-');
+		num = -num;
+		i = 1;
+	}
+	if (num > 0)
+	{
+		for (div = 1; div <= num; div *= 10)
+			;
+		while (div > 1)
+		{
+			div /= 10;
+			_putchar((num / div) + '0');
+			num %= div;
+			i++;
+		}
+	}
+	return (i);
 }
-else if (num < 0)
+
+int uint_print(va_list args)
 {
-_putchar('-');
-num = -num;
-i = 1;
+
+	unsigned int num, div, i = 0;
+
+	num = va_arg(args, int);
+
+	if (num == 0)
+	{
+		_putchar('0');
+		i = 1;
+	}
+	if (num > 0)
+	{
+		for (div = 1; (num / div) > 9; div *= 10)
+			;
+		while (div != 0)
+		{
+			_putchar((num / div) + '0');
+			num %= div;
+			div /= 10;
+			i++;
+		}
+	}
+	return (i);
+
 }
-if (num > 0)
+
+int oct_print(va_list args)
 {
-for (div = 1; div <= num; div *= 10)
-;
-while (div > 1)
+
+	unsigned int num, i = 0, j = 0;
+	int nums[100];
+
+	num = va_arg(args, int);
+
+	if (num < 9)
+	{
+		_putchar(num + '0');
+		i = 1;
+	}
+	else if (num >= 9)
+	{
+		while (num > 0)
+		{
+			nums[j] = num % 8;
+			num /= 8;
+			j++;
+		}
+	}
+	while (j--)
+	{
+		_putchar(nums[j] + '0');
+		i++;
+	}
+	return (i);
+}
+
+/**
+ * percent_print - Function that prints a "%" parameter passed into it
+ *
+ * @args: Variadic list of arguments
+ * Return: The number of arguments printed
+ */
+
+int percent_print()
 {
-div /= 10;
-_putchar((num / div) + '0');
-num %= div;
-i++;
-}
-}
-return (i);
+	_putchar('%');
+	return (1);
 }
