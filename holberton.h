db915a0410c72adf1b23474cfbe54c789c0705a1
paurbano <paurbano@gmail.com>
2019-10-28 15:38:27 +0000

    Add functions for Integers and percentage

--- holberton.h
+++ holberton.h
@@ -10,7 +10,10 @@ int (*function)();
 }op_print;
 
 void _putchar(char);
+int int_print(va_list);
 int str_print(va_list);
 int char_print(va_list);
+int percent_print(void);
+int _printf(const char *, ...);
 
 #endif
